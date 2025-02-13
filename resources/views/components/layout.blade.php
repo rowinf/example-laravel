23c4e0e300157847b5d709631ad0c1aeee4bd67a
rowinf <964049+rowinf@users.noreply.github.com>
2025-02-14 09:01:21 +1300

    add tailwind 3 to node_modules

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js'])
 </head>
 
 <body class="h-full">
